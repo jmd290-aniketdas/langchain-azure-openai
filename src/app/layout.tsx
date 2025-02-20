60b6104e0a0c14a0ff3142d368e95157e2723195
Aniket Das <aniketdas@jmangroup.com>
2025-02-20 17:51:31 +0530

    blank next js project bundled with bun

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,12 @@
+import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const poppins = Poppins({
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
@@ -25,9 +22,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn(
+          "antialiased border-border outline-ring/50 bg-background text-foreground",
+          poppins.className
+        )}
       >
         {children}
+        <Toaster />
       </body>
     </html>
   );
